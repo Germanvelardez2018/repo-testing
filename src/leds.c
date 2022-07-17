1ab7a0df0ea8bf7f336222aa9262166d943d81f6
German Velardez <germanvelardez16@gmail.com>
2022-07-16 21:27:14 -0300

    Correccion 6: Se creoo funcion Param_check para evitar repetir codigo. Esta funcion valida los parametros ingresados a las funciones

--- src/leds.c
+++ src/leds.c
@@ -25,38 +25,59 @@
 #define MIN_LED_INDEX                       (1)
 
 
+// MACROS PARA CHECK
+#define CHECK_OK                            (1)
+#define CHECK_ERROR                         (0)
+
+
+
 uint16_t*          port_address;
 registro_errores_t log_error;
 
 
-void Leds_init(uint16_t* port, registro_errores_t log_error){
+
+
+static int Param_check(int index){
+    int ret = CHECK_OK;
+    if(index > MAX_LED_INDEX || index < MIN_LED_INDEX)
+    {
+        ret = CHECK_ERROR;
+        log_error(ALERTA,__FUNCTION__,__LINE__,ALERT_MSG_INVALID_PARAM);
+    } 
+    return ret;
+}
+
+
+
+
+void Leds_init(uint16_t* port, registro_errores_t reg_error){
     
     port_address = port;
-    log_error = log_error;
+    log_error = reg_error;
     Led_turn_off_all(); 
 }
 
 
 void Led_turn_on(int led_position){
 
-    if(led_position > MAX_LED_INDEX || led_position < MIN_LED_INDEX){
-        log_error(ALERTA,__FUNCTION__,__LINE__,ALERT_MSG_INVALID_PARAM);
-    }
-    else{
-        (*port_address) |= SET_BIT((*port_address),LED_OFFSET(led_position));
-    }
-};
+    if(Param_check(led_position))    (*port_address) |= SET_BIT((*port_address),LED_OFFSET(led_position));    
+}
 
 
 void Led_turn_off(int led_position){
 
-    (*port_address) |= CLR_BIT((*port_address),LED_OFFSET(led_position));
+    if(Param_check(led_position)) (*port_address) |= CLR_BIT((*port_address),LED_OFFSET(led_position));
 }
 
 
 int Led_check_state(int led_position){
-
-    return  (BIT_GET((*port_address),LED_OFFSET(led_position)))?LED_ON:LED_OFF;
+    
+    int ret = LED_OFF;
+     if(Param_check(led_position)) 
+     {
+        if(BIT_GET((*port_address),LED_OFFSET(led_position))) ret = LED_ON;
+     }
+     return ret;
 }
 
 
